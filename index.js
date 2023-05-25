bb5f910d3937d982f649d878cb54321cc90f9977
arreejj <areej2102776@miuegypt.edu.eg>
2023-05-25 23:15:45 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); //athkm f static folder
+app.use(bodyParser.json());
+////////////////////////////
+app.set("view engine", "ejs");
+app.use(express.static("public"));
