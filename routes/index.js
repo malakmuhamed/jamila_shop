0f5201d1bf8094b241dd85635b6c9735618c1f8a
arreejj <areej2102776@miuegypt.edu.eg>
2023-05-25 23:40:06 +0300

    index js

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,24 @@ const {
   verifyAccessToken,
 } = require("../middleware/authentication.middleware");
 
+const { authSchema } = require("../middleware/validation-Schema");
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const valid = await authSchema.validateAsync(req.body);
+    const action = await User.findOne({ email: valid.email });
+    if (action)
+      throw createError.Conflict(`username ${valid.email} created before`);
+    const user = new User(valid);
+    const signup = await user.save();
+    const accesstoken = await signAccessToken(user.id);
+    req.session.token = accesstoken;
+    req.session.fullname = signup;
+    const usertype = req.session.fullname.typeofuser;
+        if (usertype == "user") {
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
