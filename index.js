5a6cc1ef230482491e547ec984acd54f880589a1
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-05-26 11:26:45 +0300

    index

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ app.use(bodyParser.json());
 ////////////////////////////
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(morgan("common"));
+require("dotenv").config();
+
+app.use(fileUpload());
