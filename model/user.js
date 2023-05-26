5e3e61209a911f947807af15a5bdedbcd97bba7f
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-05-26 10:55:12 +0300

    userschema

---
+++ model/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const UserSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    lowercase: true,
+    unique: true,
+  },
+  
+});
