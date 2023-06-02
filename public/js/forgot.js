c67268264ffe26c89a187c2be7b9eb64e385163f
Adham Said <adham.soliman725@gmail.com>
2023-06-02 16:15:47 +0300

    creating the token

--- public/js/forgot.js
+++ public/js/forgot.js
@@ -13,11 +13,12 @@ let user={
     email:"adham.soliman725@gmail.com",
     password:"########",
 };
-app.get('/',(req,res)=>
-{
-    res.send("Hello World");
-});
 
+app.get('/forget-password',(req,res,next)=>
+    {res.render('forget-password');}
+    );
+    app.post('/forget-password',(req,res,next)=>
+    { const {email}=req.body;
 
  //Creating a one time link for a period of time.
  const secret= JWTsecret+user.password;              //JWT is common but password isn't so it will be unique for each user.
@@ -25,3 +26,11 @@ app.get('/',(req,res)=>
      email:user.email,
      id:user.password,
  };   
+
+ //Creating the token.
+ const token=jwt.sign(payload,secret,{expiresIn: '15m'});//expiresIn is a function provided by jsonwebtoken.
+ const link=`http://localhost:3000/reset-password/${user.id}/${token}`;
+ console.log(link);
+ res.send('password reset link has been sent to your email');
+ }
+ );
