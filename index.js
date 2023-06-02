d049e58a230fc1312b324d4d75943d93b25d0ec3
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-06-02 22:16:21 +0300

    admin_controllers

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ const adminRouter = require("./routes/admin");
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/jamila");
-
+  await mongoose.connect("mongodb+srv://malak2102056:56850906@cluster0.da8eto8.mongodb.net/jamila");
   console.log("Connected With DB");
 }
 
