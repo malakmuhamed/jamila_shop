fd15ffc835e76b8aef16e489b881a6762c37642d
shahd soliman <shahd2101760@miuegypt.edu.eg>
2023-06-07 07:17:36 +0300

    a

--- controllers/views_controllers.js
+++ controllers/views_controllers.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var createError = require("http-errors");
 var app = express();
+var router = express.Router();
 var imgSchema = require("../model/product");
 var UsersSchema = require("../model/user");
-
-const offers=(req, res) => {
-  imgSchema.find({offer : {$gt: 0}}).then((data, err) => {
+var orderSchema = require("../model/order");
+const offers = (req, res) => {
+  imgSchema.find({ offer: { $gt: 0 } }).then((data, err) => {
     if (err) {
       console.log(err);
     }
@@ -17,24 +18,24 @@ const offers=(req, res) => {
   });
 };
 
-  const signup_views=(req, res) => {
-    res.render("signup", {
-      token: req.session.token === undefined ? "" : req.session.token,
-      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-    });
-  };
-  const signin_views= (req, res) => {
-    res.render("signin", {
-      token: req.session.token === undefined ? "" : req.session.token,
-      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-    });
-  };
+const signup_views = (req, res) => {
+  res.render("signup", {
+    token: req.session.token === undefined ? "" : req.session.token,
+    fullname: req.session.fullname === undefined ? "" : req.session.fullname,
+  });
+};
+const signin_views = (req, res) => {
+  res.render("signin", {
+    token: req.session.token === undefined ? "" : req.session.token,
+    fullname: req.session.fullname === undefined ? "" : req.session.fullname,
+  });
+};
 
 
 
-const search=async (req, res, next) => {
+const search = async (req, res, next) => {
   const { searchtext } = req.params;
-  imgSchema.find({ name:  { $regex: searchtext } }).then((data, err) => {
+  imgSchema.find({ name: { $regex: searchtext } }).then((data, err) => {
     if (err) {
       console.log(err);
     }
@@ -47,54 +48,54 @@ const search=async (req, res, next) => {
 }
 
 
-const product_views= (req, res) => {
-    imgSchema.find({}).then((data, err) => {
-      if (err) {
-        console.log(err);
-      }
-      res.render("product", {
-        items: data,
-        token: req.session.token === undefined ? "" : req.session.token,
-        fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-      });
+const product_views = (req, res) => {
+  imgSchema.find({}).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("product", {
+      items: data,
+      token: req.session.token === undefined ? "" : req.session.token,
+      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
     });
-  };
+  });
+};
 
 
-  const product_details= (req, res) => {
-    imgSchema.findById(req.params.id).then((data, err) => {
-      if (err) {
-        console.log(err);
-      }
-      res.render("product_details", {
-        items: data,
-        token: req.session.token === undefined ? "" : req.session.token,
-        fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-      });
+const product_details = (req, res) => {
+  imgSchema.findById(req.params.id).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("product_details", {
+      items: data,
+      token: req.session.token === undefined ? "" : req.session.token,
+      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
     });
-  };
-  
-  
-  const product_category= (req, res) => {
-    const { category } = req.params;
-    imgSchema.find({ category: category }).then((data, err) => {
-      if (err) {
-        console.log(err);
-      }
-      res.render("productcategory", {
-        items: data,
-        token: req.session.token === undefined ? "" : req.session.token,
-        fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-      });
+  });
+};
+
+
+const product_category = (req, res) => {
+  const { category } = req.params;
+  imgSchema.find({ category: category }).then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("productcategory", {
+      items: data,
+      token: req.session.token === undefined ? "" : req.session.token,
+      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
     });
-  };
-  
-module.exports={
-   offers,
-   signup_views,
-   signin_views,
-   product_views,
-   product_details,
-   product_category,
-    search
+  });
+};
+
+module.exports = {
+  offers,
+  signup_views,
+  signin_views,
+  product_views,
+  product_details,
+  product_category,
+  search
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,28 +3,14 @@ var createError = require("http-errors");
 var router = express.Router();
 const UsersSchema = require("../model/user");
 var imgSchema = require("../model/product");
+var orderSchema = require("../model/order");
 var app = express();
 var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false })); //athkm f static folder
 router.use(bodyParser.json());
 
 const { authSchema } = require("../middleware/validation-Schema");
-
-////////////////////////////////
-var multer = require("multer");
-var fs = require("fs");
-const path = require("path");
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads"); //eh cb de
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
-var upload = multer({ storage: storage });
-
-////////////////////////////
+const admin = require("../controllers/admin_controllers");
 
 router.use((req, res, next) => {
   if (req.session.fullname !== undefined && req.session.fullname.typeofuser === 'admin') {
@@ -32,30 +18,11 @@ router.use((req, res, next) => {
   } else {
     res.redirect('/')
   }
-})
+});
 
+var fs = require("fs");
+const path = require("path");
 
-// router.post("/addproduct", upload.single("image"), (req, res) => {
-//   var obj = {
-//     name: req.body.name,
-//     category: req.body.category,
-//     price: req.body.price,
-//     desc: req.body.desc,
-//     img: {
-//       data: fs.readFileSync(
-//         path.join(__dirname + "/uploads/" + req.file.filename)
-//       ),
-//       contentType: "image/png",
-//     },
-//   };
-//   imgSchema.create(obj).then((err, item) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//     }
-//     res.redirect("/admin");
-//   });
-// });
 
 router.get("/addproduct", (req, res) => {
   imgSchema.find({}).then((data, err) => {
@@ -72,33 +39,7 @@ router.get("/addproduct", (req, res) => {
 
 ///////////////////////////////////
 
-router.post("/addproduct2", (req, res) => {
-  let imgFile;
-  let uploadPath;
-  if (!req.files) {
-    return res.status(400).send("No files were uploaded.");
-  }
-  imgFile = req.files.img;
-  uploadPath = __dirname + "/public/uploads/" + req.body.name + ".png";
 
-  imgFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err);
-  });
-  var obj = {
-    name: req.body.name,
-    category: req.body.category,
-    price: req.body.price,
-    desc: req.body.desc,
-    img: req.body.name + ".png",
-  };
-  imgSchema.create(obj).then((err, item) => {
-    if (err) {
-      console.log("");
-    } else {
-    }
-    res.redirect("/admin");
-  });
-});
 
 
 router.post("/addproduct3", (req, res) => {
@@ -134,92 +75,18 @@ router.post("/addproduct3", (req, res) => {
   });
 });
 
-router.get("/", (req, res) => {
-  res.render("admin_dashboard", {
-    token: req.session.token === undefined ? "" : req.session.token,
-    fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-  });
-});
 
-router.get("/customers", (req, res) => {
-  UsersSchema.find({ typeofuser: "user" }).then((data, err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.render("admin_customers", {
-      items: data,
-      token: req.session.token === undefined ? "" : req.session.token,
-      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-    });
-  });
-});
 
-router.get("/admins", (req, res) => {
-  UsersSchema.find({ typeofuser: "admin" }).then((data, err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.render("admin_customers", {
-      items: data,
-      token: req.session.token === undefined ? "" : req.session.token,
-      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-    });
-  });
-});
 
-router.get("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-  await UsersSchema.findByIdAndDelete(id);
-  res.redirect("/admin/customers");
-});
 
-router.get("/changeuser/:id", async (req, res) => {
-  const { id } = req.params;
-  const Typeofuser = await UsersSchema.findById(id).then(async (data, err) => {
-    if (err) {
-      console.log(err);
-    }
-    if (data.typeofuser == "admin") {
-      await UsersSchema.findByIdAndUpdate(id, { typeofuser: "user" });
-      res.redirect("/admin");
-    } else if (data.typeofuser == "user") {
-      await UsersSchema.findByIdAndUpdate(id, { typeofuser: "admin" });
-      res.redirect("/admin/customers");
-    }
-  })
-});
 
-router.get("/product", (req, res) => {
-  imgSchema.find({}).then((data, err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.render("admin_product", {
-      items: data,
-      token: req.session.token === undefined ? "" : req.session.token,
-      fullname: req.session.fullname === undefined ? "" : req.session.fullname,
-    });
-  });
-});
-
-router.post('/updateproduct/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const action = await imgSchema.findByIdAndUpdate(id, req.body);
-    res.redirect("/admin/product")
-  } catch (error) {
-    console.log(error);
-  }
-
-})
-router.post('/deleteproduct/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const action = await imgSchema.findByIdAndDelete(id, req.body);
-    res.redirect("/admin/product")
-  } catch (error) {
-    console.log(error);
-  }
-});
-
+router.get("/cartviews", admin.cartviews);
+router.get("/", admin.admin_dashboard);
+router.get("/admins", admin.get_admins);
+router.get("/customers", admin.get_customers);
+router.get("/delete/:id", admin.delete_customers);
+router.get("/changeuser/:id", admin.changeuser);
+router.get("/product", admin.viewproduct);
+router.post('/updateproduct/:id', admin.updateproduct);
+router.get('/deleteproduct/:id', admin.deleteproduct);
 module.exports = router;
