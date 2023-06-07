7b3199a95dce0e588631bfe33f7764f4b1f23537
shahd soliman <shahd2101760@miuegypt.edu.eg>
2023-06-07 11:23:36 +0300

    sigin

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,52 @@ const {
 const user = require("../controllers/index_controllers");
 const { authSchema } = require("../middleware/validation-Schema");
 
-router.post("/signup", user.signup);
-router.post("/signin", user.signin);
+router.post("/signup", async (req, res, next) => {
+  try {
+    const valid = await authSchema.validateAsync(req.body);//byndah 3ala authschema yt3ml valid
+    const action = await User.findOne({ email: valid.email });
+    if (action)
+      throw createError.Conflict(`username ${valid.email} created before`);
+    const user = new User(valid);
+    const signup = await user.save();
+
+
+    req.session.fullname = signup;
+    const usertype = req.session.fullname.typeofuser;
+    if (usertype == "user") {
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+});
+
+
+router.post("/signin", async (req, res, next) => {
+  try {
+    const valid = await authSchema.validateAsync(req.body);
+    const user = await User.findOne({ email: valid.email });
+    if (!user)
+      throw createError.NotFound(`username ${valid.email} Not Registered`);
+    const validPass = await user.isValidPassword(valid.password);
+    if (!validPass)
+      throw createError.Unauthorized(`username / password not valid`);
+
+    req.session.fullname = user;
+    const usertype = req.session.fullname.typeofuser;
+    if (usertype == "admin") {
+      res.redirect("/admin");
+    } else if (usertype == "user") {
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    next(error);
+  }
+});
+
+
 
 router.get("/users", verifyAccessToken, async (req, res, next) => {
   try {
